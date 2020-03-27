04c333eaa4b30ea0d191e4f51a0596fe3d9a0a25
Tomas Okal <tomas.okal@gmail.com>
2020-03-27 16:41:19 -0500

    Added optional parameter to change output to rowwise or columnwise percentages. Documentation is updated to reflect this.

--- R/svy_prop.R
+++ R/svy_prop.R
@@ -5,17 +5,51 @@
 #' @param row Row variable from survey object.
 #' @param column Column variable from survey object.
 #' @param data Survey object.
+#' @param orientation Specify whether proportions should be given rowwise or colwise.
 #'
 #' @return A dataframe with survey frequencies put into a table of proportions.
 #'
 #' @import survey
 #'
 #' @examples svy_prop(row = "b1a", column = "raceth", data = svy_df)
-svy_prop <- function(row, column, data) {
+svy_prop <- function(row, column, data, orientation = NULL) {
   
-  formula <- as.formula(paste0("~", row, "+", column))
+  formula <- as.formula(paste0("~", column, "+", row))
   df <- as.data.frame(svytable(formula, design = data, Ntotal = 100))
   df_prop <- as.data.frame(tapply(df[, 3], list(df[, 2], df[, 1]), mean))
+  
+  if (missing(orientation)) {
+    
+    df_prop <- df_prop
+    
+  }
+  
+  else {
+    
+    if (orientation == "colwise") {
+      
+      df_prop <- apply(df_prop, 2, function(x) {x / sum(x)})
+      df_prop[is.na(df_prop)] <- 0
+      
+    }
+    
+    if (orientation == "rowwise") {
+      
+      df_prop_sum <- apply(df_prop[, 1:ncol(df_prop)], 1, sum)
+      df_prop <- df_prop / df_prop_sum
+      df_prop[is.na(df_prop)] <- 0
+      
+    }
+    
+    else {
+      
+      df_prop <- df_prop
+      
+      print("Please select either rowwise or colwise for orientation.")
+      
+    }
+    
+  }
   
   return(df_prop)
   
