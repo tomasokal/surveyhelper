cc7b00f35e8fd48606f64890dcb54133a8181e2c
Tomas Okal <tomas.okal@gmail.com>
2020-04-01 18:07:59 -0500

    Some more work on svy_compare.

--- R/svy_compare.R
+++ R/svy_compare.R
@@ -8,6 +8,7 @@
 #' @return A data.frame with counts of each possible combination.
 #' 
 #' @import survey
+#' @import data.table
 #'
 svy_compare <- function(variables, data) {
   
@@ -16,3 +17,34 @@ svy_compare <- function(variables, data) {
   # Get counts of how many times each combination appears in the provided dataset.
   
 }
+
+
+library(data.table)
+library(survey)
+
+
+temp_svy$variables$pid1
+
+unique(expand.grid(temp_svy$variables$pid1, temp_svy$variables$pida))
+
+caseid <- seq(1:1000)
+weight <- rnorm(1000, 1, 0.15)
+var1 <- as.factor(sample(c("Yes", "No"), 1000, TRUE))
+
+sample_df <- as.data.frame(cbind(caseid, weight, var1))
+
+sample_df$var2 <- ifelse(sample_df$var1 == 1, sample(c("Yes", "No"), replace = TRUE), NA)
+
+svy_df <- svydesign(ids = ~ caseid,
+                    weights = ~ weight,
+                    data = sample_df)
+
+test <- svy_df[["variables"]]
+dt <- setDT(test)
+dt[
+  , 
+  .N
+  , 
+  by = .(var1, var2)
+  ]
+
