27cd6ac0d368a746f6b1290bc4139735a9728d59
Tomas Okal <tomas.okal@gmail.com>
2020-04-03 14:22:28 -0500

    Working svy_compare function :)
    Still not really sure if useful :(

--- R/svy_compare.R
+++ R/svy_compare.R
@@ -2,65 +2,48 @@
 #' 
 #' This is a helper function to get counts of how many times each combination of levels of a provided vector of variables appears in the provided dataset. Useful for tracking programmatic logic.
 #'
-#' @param variables Vector of columns from survey object.
-#' @param data Survey object.
+#' @param variables Vector of columns from data.
+#' @param data A data object that can be either a survey object or other data type.
 #'
 #' @return A data.frame with counts of each possible combination.
 #' 
 #' @import survey
 #' @import data.table
 #'
+#' @example svy_compare(variables = c("D3", "D4", "D5", "D6"), data = check)
+#' 
 svy_compare <- function(variables, data) {
   
-  dt <- setDT(data[["variables"]])
+  if (is(data, "survey.design") == TRUE | is(data, "survey.design2") == TRUE) {
   
-  dt_output <- dt[
+    dt <- setDT(data[["variables"]])
     
-    ,
-    .N
-    ,
-    by = mget(variables)
-  ]
+    dt_output <- dt[
+      
+      ,
+      .N
+      ,
+      by = mget(variables)
+      ]
+  
+  }
+  
+  else {
+    
+    dt <- setDT(data)
+    
+    dt_output <- dt[
+      
+      ,
+      .N
+      ,
+      by = mget(variables)
+      ]
+    
+  }  
+  
+  dt_output <- dt_output[do.call(order, dt_output), ]
+  
+  return(dt_output)
   
 }
-
-
-library(data.table)
-library(survey)
-
-
-temp_svy$variables$pid1
-
-unique(expand.grid(temp_svy$variables$pid1, temp_svy$variables$pida))
-
-caseid <- seq(1:1000)
-weight <- rnorm(1000, 1, 0.15)
-var1 <- as.factor(sample(c("Yes", "No"), 1000, TRUE))
-
-sample_df <- as.data.frame(cbind(caseid, weight, var1))
-
-sample_df$var2 <- ifelse(sample_df$var1 == 1, sample(c("Yes", "No"), replace = TRUE), NA)
-
-svy_df <- svydesign(ids = ~ caseid,
-                    weights = ~ weight,
-                    data = sample_df)
-
-test <- svy_df[["variables"]]
-dt <- setDT(test)
-dt[
-  , 
-  .N
-  , 
-  by = .(var1, var2)
-  ]
-dt[
-  , 
-  .N
-  , 
-  by = mget(variables)
-  ]
-
-variables = c("var1", "var2")
-check <- rlang::expr(var1)
-check <- as.name(variables)
-list(svy_df$variables$var1, svy_df$variables$var1)
