5a5a311f6eae2286847e6414bd3bd8a520825651
Tomas Okal <tomas.okal@gmail.com>
2020-04-07 14:44:12 -0500

    Have working test right now. Current implementation allows for function to run across all options of group variable.

--- R/svy_test.R
+++ R/svy_test.R
@@ -1,20 +1,77 @@
-# Necessary packages to load
+# Current scratch work
+
+## Necessary packages
 library(survey)
 library(multcomp)
+library(data.table)
 
-# Data
-data(api)
+## Data used
+df <- fread("P:\\AP-NORC Center\\Common\\AP Special Projects\\Omnibus\\2020\\03.2020\\AP March\\Data\\March_completes_clean_AP.csv")
 
-# Survey design setup
-dclus1 <- survey::svydesign(id = ~dnum, weights = ~pw, data = apiclus1, fpc = ~fpc)
+## Process
+dt <- df[, c("CaseId", "WEIGHT_ENES", "CUR1", "pid")]
+  
+  # Picking out caseid, weights, response variable, and group variable.
 
-# SVYGLM
-glm_model <- survey::svyglm(ell ~ mobility, 
+dt1 <- as.data.table(stats::model.matrix(~ CUR1 - 1, data = df))
+dt2 <- as.data.table(stats::model.matrix(~ pid - 1, data = df))
+
+  # Use model.matrix to create sets of dummy variables.
+
+    # Issue exists that this adds in the variables as they appear in dataset.
+
+dt_model <- as.data.table(cbind(dt[, -c(3, 4)], dt1, dt2))
+
+  # cbind these outputs to the orginal dataframe where we also drop the original variables.
+
+dclus1 <- survey::svydesign(id = ~ CaseId, weights = ~ WEIGHT_ENES, data = dt_model)
+
+  # Generate survey design object.
+
+cols <- grep("pid", names(dt_model))
+
+  # Pick out columns that have the group variable in them.
+
+group_var <- as.list(names(dt_model[, ..cols]))
+
+  # Assign these into a list.
+    
+    # Will want more efficient way to do this.
+
+## Funcitonal process
+
+list_m <- lapply(group_var, function(x) svyglm(as.formula(paste0("`CUR1Right direction` ~ ", x)), design = dclus1))
+
+  # Run svyglm over list of group variables. 
+
+list_c <- lapply(list_m, function(x) summary(multcomp::glht(x)))
+
+  # Put these models through multiple comparison function.
+
+list_p <- lapply(list_c, function(x) as.data.frame(x[[9]][6]))
+
+  # Extract p-values from these.
+
+
+## Scrath work below
+
+df$cur1_rightdirection <- ifelse(df$CUR1 %in% "Right direction", 1, 0)
+df$cur1_wrongdirection <- ifelse(df$CUR1 %in% "Wrong direction", 1, 0)
+df$pid_dem <- ifelse(df$pid %in% "Democrat", 1, 0)
+df$pid_rep <- ifelse(df$pid %in% "Republican", 1, 0)
+df$pid_ind <- ifelse(df$pid %in% "Independent", 1, 0)
+
+dclus1 <- survey::svydesign(id = ~ CaseId,
+                            weights = ~ WEIGHT_ENES,
+                            data = dt_model)
+
+glm_model <- survey::svyglm(cur1_rightdirection ~ pid_dem, 
                             design = dclus1)
 
 pw <- summary(multcomp::glht(glm_model))
 pairwise.comps <- data.frame(names(pw$test$coefficients), pw$test$coefficients, pw$test$pvalues)
 
+### Ben scratch work
 
 design.RDDphase <- IISclaf_df %>% as_survey_design(ids = CASEID,
                                                    nest = TRUE,
